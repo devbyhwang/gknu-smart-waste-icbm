6b50addc44876cf0fcabffd64f57c4d241b5ef81
psw36 <pyshinwon2014@gmail.com>
2026-05-29 13:54:18 +0900

    motor3
    모터 떨림 관련 수정1

---
+++ hardware-motor3.py
@@ -0,0 +1,70 @@
+from enum import Enum
+from time import sleep
+from gpiozero import AngularServo
+from gpiozero.pins.lgpio import LGPIOFactory
+
+factory = LGPIOFactory()
+
+class WasteCategory(Enum):
+    CAN = "Can"
+    PLASTIC = "Plastic"
+    GLASS = "Glass"
+    PAPER = "Paper"
+    UNKNOWN = "Unknown"
+
+class MotorC:
+    def __init__(self, bottom_pin=18, top_pin=19):
+        self.bottom_servo = AngularServo(
+            bottom_pin, min_angle=0, max_angle=180, 
+            min_pulse_width=0.0005, max_pulse_width=0.0025, pin_factory=factory
+        )
+        
+        self.top_servo = AngularServo(
+            top_pin, min_angle=0, max_angle=180, 
+            min_pulse_width=0.0005, max_pulse_width=0.0025, pin_factory=factory
+        )
+        
+        self.angle_map = {
+            WasteCategory.PAPER:   (90, 135),
+            WasteCategory.UNKNOWN: (90, 135),
+            WasteCategory.CAN:     (90, 45),
+            WasteCategory.PLASTIC: (15, 135),
+            WasteCategory.GLASS:   (15, 45)
+        }
+        
+        self.reset_motors()
+
+    def reset_motors(self):
+        # 1. 모터를 초기 위치로 보냄
+        self.bottom_servo.angle = 0
+        self.top_servo.angle = 90
+        sleep(1.0) # 도착할 때까지 1초 기다림
+        
+        # 2. 도착 후 신호 끄기 (모터 떨림/소음 완벽 방지)
+        self.bottom_servo.value = None
+        self.top_servo.value = None
+
+    def process_item(self, received_value):
+        try:
+            category = WasteCategory(received_value)
+        except ValueError:
+            category = WasteCategory.UNKNOWN
+
+        bottom_angle, top_angle = self.angle_map.get(category, (90, 135))
+        
+        print(f"\n[모터 구동] '{category.value}' 분류를 시작합니다.")
+
+        # [단계 2] 하단 모터 구동
+        print(f" 1. 하단 모터 {bottom_angle}도로 이동")
+        self.bottom_servo.angle = bottom_angle
+        sleep(1.0)
+
+        # [단계 3] 상단 모터 구동
+        print(f" 2. 상단 모터 {top_angle}도로 이동 (투하)")
+        self.top_servo.angle = top_angle
+        sleep(1.0)
+
+        # [단계 4] 초기 위치로 복귀
+        print(" 3. 모터 초기화 및 신호 차단")
+        self.reset_motors()
+        print("[분류 완료]")
