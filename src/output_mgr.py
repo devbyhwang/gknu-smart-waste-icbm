bf24ae7b2881c3269f3b2e7026a15fb01eff3d5f
hwang <devbyhwang@gmail.com>
2026-06-02 19:19:08 +0900

    Ignore ultrasonic refresh during sorting

--- src/output_mgr.py
+++ src/output_mgr.py
@@ -35,6 +35,9 @@ class OutputM(HandleClassificationResult):
         self._polling_stop = threading.Event()
         self._polling_thread = None
         self._alerted_full_bins = set()
+        self._motor_active = False
+        self._last_fill_levels = {}
+        self._last_full_bins = set()
 
         if enable_sensor_polling:
             self.start_sensor_polling()
@@ -216,11 +219,20 @@ class OutputM(HandleClassificationResult):
 
     def refreshSensorStatus(self):
         with self._state_lock:
+            if self._motor_active:
+                return dict(self._last_fill_levels), set(self._last_full_bins)
+
             fill_levels = self.collectFillLevels()
             full_bins = self.collectFullBins(fill_levels)
+            self._last_fill_levels = dict(fill_levels)
+            self._last_full_bins = set(full_bins)
             self._show_sensor_snapshot(fill_levels, full_bins)
             self._send_new_full_bin_alert(full_bins)
             return fill_levels, full_bins
+
+    def _set_motor_active(self, active: bool):
+        with self._state_lock:
+            self._motor_active = bool(active)
 
     def _sensor_polling_loop(self):
         while not self._polling_stop.is_set():
@@ -293,6 +305,9 @@ class OutputM(HandleClassificationResult):
                 if self._read_sensor_full_status(getattr(self, "sensor", None)):
                     full_bins.add(result.label)
 
+                self._last_fill_levels = dict(fill_levels)
+                self._last_full_bins = set(full_bins)
+
                 if full_bins:
                     print(f"[OutputM] 분류 중단: 가득 찬 분류함={', '.join(item.value for item in full_bins)}")
                     self._block_classification_for_full_bin(result, fill_levels, full_bins)
@@ -324,16 +339,20 @@ class OutputM(HandleClassificationResult):
                     legacy_play_tts(result.label.value)
 
             process_item = getattr(self.servo, "process_item", None)
-            if callable(process_item):
-                process_item(result.label.value)
-            else:
-                rotate_to = getattr(self.servo, "rotateTo", None)
-                if callable(rotate_to):
-                    rotate_to(90)
+            self._set_motor_active(True)
+            try:
+                if callable(process_item):
+                    process_item(result.label.value)
                 else:
-                    legacy_rotate_to = getattr(self.servo, "rotate_to", None)
-                    if callable(legacy_rotate_to):
-                        legacy_rotate_to(90)
+                    rotate_to = getattr(self.servo, "rotateTo", None)
+                    if callable(rotate_to):
+                        rotate_to(90)
+                    else:
+                        legacy_rotate_to = getattr(self.servo, "rotate_to", None)
+                        if callable(legacy_rotate_to):
+                            legacy_rotate_to(90)
+            finally:
+                self._set_motor_active(False)
         except Exception:
             self.handleException()
 
